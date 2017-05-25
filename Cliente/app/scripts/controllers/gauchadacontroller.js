42767d6ff3d3217c39cb9d987a867e0a39f39199
DESKTOP-102PCKS\ToTO <toto_lp77@hotmail.com>
2017-05-25 20:13:01 -0300

    Cambio en vista de main y gauchadacontroller

--- Cliente/app/scripts/controllers/gauchadacontroller.js
+++ Cliente/app/scripts/controllers/gauchadacontroller.js
@@ -8,7 +8,22 @@
  * Controller of the clienteApp
  */
 angular.module('clienteApp')
-  .controller('GauchadacontrollerCtrl', function ($scope, gauchadaservice) {
+  .controller('GauchadacontrollerCtrl', function ($scope, gauchadaservice, $window) {
+    $scope.tituloAct = "";
+    $scope.descAct = "";
+    $scope.gauchadas = [
+        { titulo: "Ayuda!", descripcion: "Como obtengo el valor que retorna una funcion en AngularJS?", imagen:"https://yt3.ggpht.com/-FJXWJ1x1bEQ/AAAAAAAAAAI/AAAAAAAAAAA/ZtyuZ-elFr4/s900-c-k-no-mo-rj-c0xffffff/photo.jpg" },
+        { titulo: "Bue", descripcion: "Esta gauchada es inventada", imagen:"https://i.blogs.es/85042c/as17-148-22727/650_1200.jpg" },
+        
+        
+
+    ];
+    $scope.ver = function(){
+      $scope.tituloAct = $scope.gauchadas.titulo;
+      $scope.descAct = $scope.gauchadas.descAct;
+      $window.location.href = "http://localhost:9000/#!/gauchada";
+
+    }
     $scope.registrar = function(){
       usuarioService.registrarUsuario($scope.usuario)
       .then(function(vals){
