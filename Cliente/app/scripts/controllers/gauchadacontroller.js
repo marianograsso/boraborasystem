1a701a5951b1e20761fb0e3b9eca4b49ec55e94f
DESKTOP-102PCKS\ToTO <toto_lp77@hotmail.com>
2017-05-31 18:04:28 -0300

    Modificado el controler de gauchada

--- Cliente/app/scripts/controllers/gauchadacontroller.js
+++ Cliente/app/scripts/controllers/gauchadacontroller.js
@@ -18,22 +18,20 @@ angular.module('clienteApp')
     });
 
 
-    $scope.ver = function () {
+    /*$scope.ver = function () {
       $scope.tituloAct = $scope.gauchadas.titulo;
       $scope.descAct = $scope.gauchadas.descAct;
       $window.location.href = "http://localhost:9000/#!/gauchada";
 
-    }
+    }*/
     $scope.publicarGauchada = function (gauchada) {
-      if ($scope.gauchada.fechalimite < $scope.fecha) {
+      if ($scope.gauchada.FechaFin < $scope.fecha) {
         $scope.error = true;
         $scope.errormessage = "La fecha tiene que ser mayor o igual a la de hoy";
       }
       else {
-        $scope.gauchada.ActorId = null;
         $scope.gauchada.AutorId = $rootScope.usuario.id;
         $scope.gauchada.FechaInicio = new Date();
-        $scope.gauchada.FechaFin = $scope.gauchada.fechalimite.toUTCString();
         gauchadaservice.registrarGauchada($scope.gauchada)
           .then(function (vals) {
             $window.location.href = "#!/";
